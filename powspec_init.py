62e3918a761c19c667df7c2cbce39920b91c13ef
polyphant1 <chrisl1991@hotmail.co.uk>
2013-07-08 17:22:03 +0100

    powspec init: added power spectrum method with WMAP7 initial parameters

--- powspec_init.py
+++ powspec_init.py
@@ -7,51 +7,53 @@ produces a usable power spectrum from CAMB files or the Eisenstein & Hu analytic
 
 """
 
-#from ..CosmoloPy.cosmolopy.EH import power as power
-#from ..CosmoloPy.cosmolopy.EH import tf_fit as tf_fit
-
+# Cosmolopy package
 from cosmolopy.EH import power as power
 from cosmolopy import perturbation as pert
-from cosmology import *
+from cosmolopy import constants as const
 
-def import_powspec():
-  return NONE
-  
-  
-  
+from cosmology import * # IH cosmology class
 
-def filter_function_W():
-  return NONE
-  
-  
+from numpy import sqrt, log, exp, fabs, pi
 
-def standard_deviation_sigma():
-  return NONE
-  
-  
 
-def variance_S():
+def import_transfer_function():
+  #import a transfer function from a CAMB produced output file
   return NONE
-  
-  
 
 def transfer_function_EH(k,z=0.0,cosm=Cosmology()):
   # set cosmology
   power.TFmdm_set_cosm(cosm.O_m0,cosm.O_b0,-1,0,cosm.O_de0,cosm.h_0,z)
   
-  # calculate transfer function given wavenumber
+  # Call to EH power.c script
+  # Calculates transfer function given wavenumber
   return power.TFmdm_onek_mpc(k)
   
 
-def growth_factor_D(k,z=0.0,cosm=Cosmology()):
+def growth_factor_D(z=0.0,cosm=Cosmology()):
   # set cosmology
   power.TFmdm_set_cosm(cosm.O_m0,cosm.O_b0,-1,0,cosm.O_de0,cosm.h_0,z)
   
+  # call to cosmolopy perturbation.py script
+  # Calculates growth factor 
   return pert.fgrowth(z,cosm.O_m0,unnormed=False)
   
 
+def power_spectrum_P(k,Tk,Dz,z=0.0,cosm=Cosmology()):
+  # WMAP7 parameters:
+  delta_h = 2.43 * (10**-9)
+  n = 0.966
+  
+  return ((delta_h**2)*2*(pi**2)*(k**n)) * (const.c_light_Mpc_s/cosm.h_0)**(3+n) * (Tk/Dz)**2
+
 if __name__ == "__main__":
-  print transfer_function_EH(0.23861)
+  Tk = transfer_function_EH(0.23861)
+  Dz = growth_factor_D(z=3.0)
+  Pk = power_spectrum_P(0.23861,Tk,Dz)
+  
+  print "T(k) = %s" % Tk
+  print "D(z) = %s" % Dz
+  print "P(k) = %s" % Pk
   
   
 
